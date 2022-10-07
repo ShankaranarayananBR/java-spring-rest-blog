38a4bad9c6247ae2571145126e0cd2be394336a8
Shankaranarayanan Ramalingam <brshankar1992@gmail.com>
2022-10-07 14:27:18 +0200

    Included-version-and-validation

--- src/main/java/com/pluralsight/blog/model/Post.java
+++ src/main/java/com/pluralsight/blog/model/Post.java
@@ -3,6 +3,8 @@ package com.pluralsight.blog.model;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -13,8 +15,12 @@ import java.util.Date;
 @Entity
 public class Post {
     @Id
+
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Version private Long version;
+    @NotNull
+    @Size(min = 4, max = 100)
     private String title;
     @Column(length=1000000)
     @Lob
